c4b8ea15654289b15a80963b0e9447e30afc2132
Daniel Heitz <overclocker710@outlook.com>
2023-05-04 16:38:27 -0400

    Added docstrings to model and controller

--- controller.py
+++ controller.py
@@ -1,7 +1,29 @@
 import pygame
 class Controller:
+    """
+    Controller class. Contains the method keyboard
+    which is a key listener that just passes out what
+    keys it reads in dictionary form.
+    """
     def __init__(self, model):
+        """
+        Constructor for the Controller class
+
+        Args:
+            None
+        Returns:
+            None
+        """
         self._model = model
 
     def keyboard(self):
+        """
+        Key listener. Returns a dictionary of the keys
+        pressed. Nothing else.
+
+        Args:
+            None
+        Returns:
+            A dictionary of the pressed keys
+        """
         return pygame.key.get_pressed()
