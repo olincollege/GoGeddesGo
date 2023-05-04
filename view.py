4c63d01f92ab33490b2152b95875adbbaa3f42a9
Ava Possidente <apossidente@olin.edu>
2023-05-04 16:36:57 -0400

    Added doc strings to view.py

--- view.py
+++ view.py
@@ -13,27 +13,36 @@ from pygame.locals import (
     QUIT,
 )
 
-class View():
+
+class View:
     """
     Handles the visuals and displays of the game.
     Attributes:
         _model: A Model instance representing the model state to display.
     """
+
     def __init__(self, model):
+        """
+        Constructor for the View class.
+
+        Args:
+            _model: A Model instance representing the model state to display.
+        Returns:
+            None
+        """
         self._model = model
 
-    def start_screen(self):
-        pass
-
-    def win_screen(self):
-        pass
-
-    def game_over(self):
-        pass
-
     def draw_game(self):
+        """
+        Renders the current frame of the game based on the
+        state contained in the model
+
+        Args:
+            None
+        Returns:
+            None
+        """
         self._model._screen.blit(self._model.bg, (0, 0))
         for entity in self._model.all_sprites:
             self._model._screen.blit(entity.surf, entity.rect)
         pygame.display.flip()
-        
